a86f2af023d3a90478ca2cf9b4793ea665cb628b
iamundersalami <134235854+iamundersalami@users.noreply.github.com>
2024-03-17 22:39:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+list = ['rock', 'paper', 'scissors']
+
+
+def main():
+    def loose():
+        print("You loose")
+    player = input("============================================="
+                   "\nEnter: rock, paper or scissors: ")
+    if player not in list:
+        print("Error")
+    else:
+        computer = random.choice(list)
+        print(f'You: {player}\nMe: {computer}')
+        if player == computer:
+            print('draw')
+        elif player == 'rock' and computer == 'paper':
+            loose()
+        elif player == 'paper' and computer == 'scissors':
+            loose()
+        elif player == 'scissors' and computer == 'rock':
+            loose()
+        else:
+            print('You win')
+
+
+while True:
+    main()
